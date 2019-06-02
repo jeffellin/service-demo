e4438d0fb05030815d745ddf1b707df0450f4c10
Jeffrey Ellin <jellin@pivotal.io>
2019-06-01 21:42:17 -0400

    version

--- src/main/java/com/example/ellin/webdemo/repository/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ellin.webdemo.repository;
-
-import com.example.ellin.webdemo.Hello;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends CrudRepository<Hello,String> {
-}
